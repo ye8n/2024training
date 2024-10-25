3bcd918e82cdc70a9f5180a08a97a3b81952ecbf
ye8n <skyeon7@gmail.com>
2024-10-25 13:18:04 +0900

    20241025 백업

---
+++ html/20241025/javaScriptBasic.js
@@ -0,0 +1,13 @@
+
+//1. JS 기본 구조:
+document.querySelector('body').innerHTML = 'Hello JavaScript!';
+// Object.method(parament); 객체.메서드(매개변수);
+
+
+// 2. 크롬 개발자도구(F12)의 콘솔 영역에 출력
+console.log('메시지 출력'); 
+//중간중간 콘솔로그를 삽입해서, 어디에서 문제가 발생하는지 파악 가능
+
+console.debug('log와 마찬가지로, 콘솔 영역에 출력')
+
+console.clear('콘솔 지우개')
